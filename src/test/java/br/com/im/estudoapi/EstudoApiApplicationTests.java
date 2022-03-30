43c15282190921944663b605de441175efa2e6ff
Igor Martins <dev.igor.martins@gmail.com>
2022-03-30 16:59:27 -0300

    Projeto criado

---
+++ src/test/java/br/com/im/estudoapi/EstudoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.im.estudoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
